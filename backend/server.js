bf890580aba440ef0b8ceee92c80ca712e3e0f60
rehankhan008 <rehanrocks555@outlook.com>
2024-09-27 18:56:16 +0530

    monitoring

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,48 @@
+#const express = require("express");
+#const cors = require('cors');
+#const mongoose = require("mongoose");
+#const port = 3001;
+#const routes = require("./routes");
+
+#main().catch((err) => console.log(err));
+
+#async function main() {
+#  await mongoose.connect("mongodb://mongo:27017/todos", {
+	#    useUnifiedTopology: true,
+	#    useNewUrlParser: true,
+	# });
+	#const app = express();
+	#app.use(cors());
+	#app.use(express.json());
+	# app.use("/api", routes);
+
+	#  app.listen(port, () => {
+		#    console.log(`Server is listening on port: ${port}`);
+		#  });
+	#i}
+
+
+
+
 const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const port = 3001;
 const routes = require("./routes");
 
+// Load environment variables
+const mongoUsername = process.env.MONGO_USERNAME;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoHost = "mongo"; // Assuming your MongoDB service is named 'mongo'
+const mongoPort = 27017;
+const mongoDbName = "todos"; // Your database name
+
+const mongoConnectionString = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDbName}`;
+
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://mongo:27017/todos", {
+  await mongoose.connect(mongoConnectionString, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
@@ -20,3 +55,4 @@ async function main() {
     console.log(`Server is listening on port: ${port}`);
   });
 }
+
